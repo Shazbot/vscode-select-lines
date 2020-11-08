56c7d89a1d395e206d8d56f0cca15b4321e5cb09
Tibor Ilić <tilic5@gmail.com>
2020-11-08 13:07:30 +0100

    now using an Input Box

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## [0.2.0]
+
+- Now using an Input Box to ask the user for number of lines to choose.
+
 ## [0.1.0]
 
 - Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -1,75 +1,61 @@
 "use strict";
 import * as vscode from "vscode";
-import { InlineInput } from "./inline-input";
-import { Selection, Position } from "vscode";
+import { window, Selection, Position } from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand("extension.selectLines", () => {
+  let disposable = vscode.commands.registerCommand("extension.selectLines", async () => {
     let editor = vscode.window.activeTextEditor;
 
     if (!editor) {
       return;
     }
 
-    let lineNumbersTemp = vscode.window.activeTextEditor!.options.lineNumbers;
-    vscode.window.activeTextEditor!.options.lineNumbers = vscode.TextEditorLineNumbersStyle.Relative;
+    let lineNumbersTemp = editor.options.lineNumbers;
 
-    new InlineInput()
-      .show(editor, v => v)
-      .then(async (value: string) => {
-        if (!value) {
-          return;
-        }
+    try {
+      editor.options.lineNumbers = vscode.TextEditorLineNumbersStyle.Relative;
 
-        let reverseDirection = false;
-        if (value === "-") {
-          reverseDirection = true;
-          value = String(
-            await new InlineInput().show(editor!, v => v).then((value: string) => {
-              return value;
-            })
-          );
+      let num_lines_as_string = (await window.showInputBox()) as string;
+      let num_lines = parseInt(num_lines_as_string.trim(), 10);
 
-          if (!value) {
-            return;
-          }
-        }
+      if (num_lines_as_string === "") {
+        num_lines = 0;
+      }
 
-        let num_lines = Number(value);
+      if (isNaN(num_lines)) {
+        return;
+      }
 
-        if (num_lines && num_lines > 0) {
-          let currentLineNum = editor!.selection.active.line;
-          if (reverseDirection) {
-            let startLineNum = currentLineNum - num_lines;
-            let startLine = editor!.document.lineAt(startLineNum);
-            let start = startLine.range.start.character + startLine.firstNonWhitespaceCharacterIndex;
-            let end = editor!.document.lineAt(currentLineNum).range.end.character;
+      let reverseDirection = num_lines < 0;
 
-            editor!.selection = new Selection(
-              new Position(startLineNum, start),
-              new Position(currentLineNum, end)
-            );
-          } else {
-            let startLineNum = currentLineNum;
-            let startLine = editor!.document.lineAt(startLineNum);
-            let start =
-              editor!.document.lineAt(currentLineNum).range.start.character +
-              startLine.firstNonWhitespaceCharacterIndex;
-            let end = editor!.document.lineAt(currentLineNum + num_lines).range.end.character;
+      let currentLineNum = editor!.selection.active.line;
+      if (reverseDirection) {
+        let startLineNum = currentLineNum + num_lines;
+        let startLine = editor!.document.lineAt(startLineNum);
+        let start = startLine.range.start.character + startLine.firstNonWhitespaceCharacterIndex;
+        let end = editor!.document.lineAt(currentLineNum).range.end.character;
 
-            editor!.selection = new Selection(
-              new Position(startLineNum, start),
-              new Position(startLineNum + num_lines, end)
-            );
-          }
-        }
-      })
-      .then(() => {
-        vscode.window.activeTextEditor!.options.lineNumbers = lineNumbersTemp;
-      })
-      .catch(() => {
-        vscode.window.activeTextEditor!.options.lineNumbers = lineNumbersTemp;
-      });
+        editor!.selection = new Selection(
+          new Position(startLineNum, start),
+          new Position(currentLineNum, end)
+        );
+      } else {
+        let startLineNum = currentLineNum;
+        let startLine = editor!.document.lineAt(startLineNum);
+        let start =
+          editor!.document.lineAt(currentLineNum).range.start.character +
+          startLine.firstNonWhitespaceCharacterIndex;
+        let end = editor!.document.lineAt(currentLineNum + num_lines).range.end.character;
+
+        editor!.selection = new Selection(
+          new Position(startLineNum, start),
+          new Position(startLineNum + num_lines, end)
+        );
+      }
+    } catch {
+    } finally {
+      vscode.window.activeTextEditor!.options.lineNumbers = lineNumbersTemp;
+    }
   });
 
   context.subscriptions.push(disposable);
